c6d3fce30b8a2002290301610fa621d7726599c8
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-21 18:46:23 +0300

    add cod

---
+++ index.js
@@ -0,0 +1,12 @@
+const string = prompt('Введите слово для проверки на палиндром');
+
+let palindrome = Array.from(string);
+let isPalindrome = true;
+
+for (let i = 0; i < palindrome.length / 2; i++) {
+    if (palindrome[i] !== palindrome[palindrome.length - 1 - i]) {
+        alert('Это не палиндром');
+        isPalindrome = false;
+        break;
+    }
+};
